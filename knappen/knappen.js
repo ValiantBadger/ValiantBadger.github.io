a079843cc7bf8e9e7eb4659fa8812d1bd47a74ad
Alvin Granlund <algr0423@storsjogymnasiet.se>
2023-10-05 11:09:07 +0200

    banagi

---
+++ knappen/knappen.js
@@ -0,0 +1,19 @@
+let siffra = 4
+
+
+
+function plusclick(){
+    //alert("hej")
+    siffra = siffra +2.569323468098754246
+    console. log(siffra)
+    document.getElementById("taltext").innerHTML = siffra
+}
+
+function minusclick(){
+   // alert("ok")
+   siffra = siffra -2.42837465859
+   console. log(siffra)
+   document.getElementById("taltext").innerHTML = siffra
+}
+
+
